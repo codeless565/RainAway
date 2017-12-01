dcdcd2af96011f911b4dc1207ff30c66508477de
PeiSheng:RE <peisheng1@hotmail.com>
2017-12-01 17:23:12 +0800

    EntityBase Changes
    Entity base will have getter and modifiers to entity's activeness and variable

--- app/src/main/java/rainaway/sidm/com/rainaway/EntityBase.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/EntityBase.java
@@ -6,10 +6,12 @@ import android.view.SurfaceView;
 
 public interface EntityBase
 {
-    boolean IsDone();
-    void SetIsDone(boolean _IsDone);
-
+    //Can get Entity's Variable and activeness.
+    //Functions
     void Init(SurfaceView _view);
     void Update(float _dt);
     void Render (Canvas _canvas);
+
+    boolean IsDone();
+    void SetIsDone(boolean _IsDone);
 }
