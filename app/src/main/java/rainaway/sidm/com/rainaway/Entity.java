8c33b8ef1752aabaefb162bbe12b3c1ae943e09a
PeiSheng:RE <peisheng1@hotmail.com>
2017-12-01 17:26:24 +0800

    Changes to Entity
    Added Life,
    Added Enum ENTITYTYPE m_type,
    Sorted out and fixed Collision Response in void OnHit() function,
    Moved codes around to ease triggering from function relations to each other.

--- app/src/main/java/rainaway/sidm/com/rainaway/Entity.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/Entity.java
@@ -3,31 +3,38 @@ package rainaway.sidm.com.rainaway;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.text.method.Touch;
 import android.view.SurfaceView;
 
 import java.util.Random;
 
-public class SampleEntity implements EntityBase, Collidable
+public class Entity implements EntityBase, EntityCollidable
 {
+    //Entity's Variable are here
+    enum ENTITYTYPE
+    {
+        ENTITY_NONE,
+        ENTITY_PLAYER,
+        ENTITY_OBSTACLE,
+        ENTITY_POWERUP
+    }
+    ENTITYTYPE m_type;
     private Bitmap bmp = null;
     private boolean isDone = false;
     private float xPos, yPos, xDir, yDir, lifeTime;
+    private int Life;
 
-    SampleEntity()
+    //Our global create function
+    //So anyone can create "SampleEntities"
+    public static Entity Create()
+    {
+        Entity result = new Entity();
+        EntityManager.Instance.AddEntity(result);
+        return result;
+    }
+
+    Entity()
     {
         //Init my variable here, that is suppose to be standardized and not changed
-    }
-
-    @Override
-    public boolean IsDone() {
-        return isDone;
-    }
-
-    @Override
-    public void SetIsDone(boolean _IsDone)
-    {
-        isDone = _IsDone;
     }
 
     @Override
@@ -69,18 +76,25 @@ public class SampleEntity implements EntityBase, Collidable
         _canvas.drawBitmap(bmp, xPos - bmp.getWidth() * 0.5f, yPos - bmp.getHeight() * 0.5f, null);
     }
 
-    //Our global create function
-    //So anyone can create "SampleEntities"
-    public static SampleEntity Create()
-    {
-        SampleEntity result = new SampleEntity();
-        EntityManager.Instance.AddEntity(result);
-        return result;
+    @Override
+    public boolean IsDone() {
+        return isDone;
     }
 
     @Override
-    public String GetType() {
-        return "SampleEntity";
+    public void SetIsDone(boolean _IsDone)
+    {
+        isDone = _IsDone;
+    }
+
+    @Override
+    public void SetEntityType(ENTITYTYPE _type) {
+        m_type = _type;
+    }
+
+    @Override
+    public ENTITYTYPE GetEntityType() {
+        return m_type;
     }
 
     @Override
@@ -99,11 +113,26 @@ public class SampleEntity implements EntityBase, Collidable
     }
 
     @Override
-    public void OnHit(Collidable _other) {
+    public void OnHit(EntityCollidable _other) {
     //COLLISION WITH OTHER ITEMS
-        if(_other.GetType() == "SampleEntity")
-        {
-            SetIsDone(true);
+        if(m_type == ENTITYTYPE.ENTITY_PLAYER)
+        {//PLAYER COLLISION RESPONSE WITH OTHER OBJECT
+            if (_other.GetEntityType() == ENTITYTYPE.ENTITY_OBSTACLE)
+            {//COLLIDED WITH OBSTACLE
+                --Life;
+                EntityBase OtherEntity = (EntityBase) _other;
+                OtherEntity.SetIsDone(true);
+            }
+            /***********************************************
+            TODO
+             - Collision with HP item
+             - Collision with Slow Time
+             - Collision with Freeze Time
+             - Collision with Shroud
+             - Collision with Slow Speed
+             - Collision with
+            ***********************************************/
+
         }
     }
 }
