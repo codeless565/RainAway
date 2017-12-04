9b61259aeb4db36ca315aa89f5364e2657eaf01a
PeiSheng:RE <peisheng1@hotmail.com>
2017-12-04 10:00:10 +0800

    Added ABC(Interface) Game_Scene

--- app/src/main/java/rainaway/sidm/com/rainaway/Game_Scene.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/Game_Scene.java
@@ -1,8 +1,15 @@
 package rainaway.sidm.com.rainaway;
 
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+
 /**
  * Created by 164347E on 12/4/2017.
  */
 
-public interface Game_Sceme {
+public interface Game_Scene {
+    void Init (SurfaceView _view);
+    void Update(float _dt);
+    void Render(Canvas _canvas);
+
 }
