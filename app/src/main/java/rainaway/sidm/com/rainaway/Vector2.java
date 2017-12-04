2cdf5f2157694de550d06dd76b553da04536b3bb
PeiSheng:RE <peisheng1@hotmail.com>
2017-12-04 12:04:28 +0800

    Vector2 Updated

--- app/src/main/java/rainaway/sidm/com/rainaway/Vector2.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/Vector2.java
@@ -4,5 +4,62 @@ package rainaway.sidm.com.rainaway;
  * Created by 164347E on 12/4/2017.
  */
 
+import java.lang.Math;
+
 public class Vector2 {
+    float x;
+    float y;
+
+    Vector2(float v)
+    {
+        x = 0.f;
+        y = 0.f;
+    }
+
+    Vector2( float a, float b )
+    {
+        x = a;
+        y = b;
+    }
+
+    Vector2(final Vector2 rhs )
+    {
+        x = rhs.x;
+        y = rhs.y;
+    }
+
+    void SetZero()
+    {
+        x = y = 0.f;
+    }
+
+    void Set( float a, float b )
+    {
+        x = a;
+        y = b;
+    }
+
+    float Length()
+    {
+        return (float)Math.sqrt(x * x + y * y);
+    }
+
+    float LengthSquared()
+    {
+        return x * x + y * y;
+    }
+
+    float Dot(Vector2 rhs )
+    {
+        return x * rhs.x + y * rhs.y;
+    }
+
+    Vector2 Normalized( )
+    {
+        float d = Length();
+        if(d == 0)
+            return new Vector2(0.f,0.f);
+
+        return new Vector2(x / d, y / d);
+    }
 }
