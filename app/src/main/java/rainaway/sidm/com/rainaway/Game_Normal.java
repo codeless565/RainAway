682aedc81630b14291fbcb6b12d3490b1790c26e
PeiSheng:RE <peisheng1@hotmail.com>
2017-12-04 10:00:51 +0800

    Game Scene change via enum at GameView

--- app/src/main/java/rainaway/sidm/com/rainaway/Game_Normal.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/Game_Normal.java
@@ -4,5 +4,41 @@ package rainaway.sidm.com.rainaway;
  * Created by 164347E on 12/4/2017.
  */
 
-public class Game_Normal {
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+
+public class Game_Normal implements Game_Scene
+{
+    public final static Game_Normal Instance = new Game_Normal(); // Singleton
+    private float timer;
+    // this is to not allow anyone else to create another game instance
+    private Game_Normal()
+    {
+
+    }
+
+    public void Init (SurfaceView _view)
+    {
+        EntityManager.Instance.Init(_view);
+        SampleBackGround.Create();
+    }
+
+    public void Update(float _dt)
+    {
+        //SAMPLE STUFF DONT DO THIS
+        timer += _dt;
+        if (timer >=0.5f)
+        {
+            Entity.Create();
+            timer = 0.f;
+        }
+
+        EntityManager.Instance.Update(_dt);
+    }
+
+    public void Render(Canvas _canvas)
+    {
+        EntityManager.Instance.Render(_canvas);
+    }
+
 }
