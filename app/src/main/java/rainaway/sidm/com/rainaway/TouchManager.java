12e79abb0fcbc67f9aa00d758db635a787532df5
Unknown <nicholasrandomz@yahoo.com.sg>
2017-12-07 18:01:25 +0800

    Removed Hold Gesture and Commented

--- app/src/main/java/rainaway/sidm/com/rainaway/TouchManager.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/TouchManager.java
@@ -6,6 +6,7 @@ import android.view.MotionEvent;
 
 public class TouchManager
 {
+    //Singleton
     public final static TouchManager Instance = new TouchManager();
 
     public enum TouchResult
@@ -13,35 +14,33 @@ public class TouchManager
         NONE,
         TAP,
         DOUBLETAP,
-        HOLD,
         SWIPELEFT,
         SWIPERIGHT,
     }
 
-    private TouchResult status = TouchResult.NONE;
+    // Class Members
+    private TouchResult status = TouchResult.NONE; // Whether gesture is Tap,Swipe,Hold
+    private Vector2 CurrTouch;      // Position of Current Touch
+    private Vector2 RecordedTouch;  // Position of Recorded Touch
+    private boolean Touching;       // Boolean for touch on screen
+    private boolean Swiping;        // Boolean for Swiping gesture
 
-    private Vector2 CurrTouch,RecordedTouch;
-    private boolean Touching,Swiping;
-    float TouchTimer;
-
-
+    // Constructor
     private TouchManager()
     {
-//        posX = 0;
-//        posY = 0;
         CurrTouch = new Vector2(0,0);
         RecordedTouch= new Vector2(0,0);
         Touching=false;
         Swiping=false;
-        TouchTimer=0.0f;
     }
 
-    //Check if have finger / click or holds
+    // Getters
     public boolean HasTouch() {return Touching;}
     public boolean getSwiping() {return Swiping;}
     public Vector2 getCurrTouch() {return CurrTouch;}
     public Vector2 getRecordedTouch() {return RecordedTouch;}
     public TouchResult getTouchResult() {return status;}
+    // Setters
     public void setCurrTouch(Vector2 _currTouch) { CurrTouch=_currTouch;}
     public void setRecordedTouch(Vector2 _RecordedTouch) {RecordedTouch=_RecordedTouch;}
     public void setTouching(boolean _Touching) {Touching =_Touching;}
@@ -49,43 +48,46 @@ public class TouchManager
 
     public void Update(MotionEvent event)
     {
-        if (event.getAction() == MotionEvent.ACTION_DOWN)
-           Touching=true;
         if (event.getAction() == MotionEvent.ACTION_UP)
             Touching=false;
-
-        if (Touching && !Swiping)
-        {
-            Swiping=true;
-            RecordedTouch=new Vector2(event.getX(),event.getY());
-            CurrTouch=RecordedTouch;
-        }
         else
+            Touching = true;
+
+        // It is always enabling swipe every touch
+        // However, it does not affect whether it is used
+        // Save the Current position of the Touch
+        if (Touching && !Swiping)                                                       // WHILE THERE IS TOUCH ON SCREEN
+        {
+                Swiping=true;
+                RecordedTouch=new Vector2(event.getX(),event.getY());
+                CurrTouch=RecordedTouch;
+        }
+        else                                                                            // WHILE THERE IS NO TOUCH ON SCREEN
         {
             Swiping=false;
             CurrTouch=new Vector2(event.getX(),event.getY());
         }
 
-        if (!Touching && status != TouchResult.NONE)
-        {
-            CurrTouch=new Vector2(0,0);
-            RecordedTouch=new Vector2(0,0);
-            TouchTimer=0.0f;
-            status = TouchResult.NONE;
-        }
-        else
-            TouchTimer+=0.1f;
+        // Conditions for Touch Status
+        // Position of touch did not change == Tap
+        // Position of exit < Position of enter == Swipe Left
+        // Position of exit > Position of enter == Swipe Right
+        if (CurrTouch.x <RecordedTouch.x)
+            status = TouchResult.SWIPELEFT;
+        else if (CurrTouch.x >RecordedTouch.x)
+            status = TouchResult.SWIPERIGHT;
+        else if (CurrTouch.x == RecordedTouch.x)
+            status = TouchResult.TAP;
 
-        if (TouchTimer>0.5f && Touching)
-            status = TouchResult.HOLD;
-        else if (TouchTimer<0.5f && Touching) // swipe left
-        {
-            if (CurrTouch.x <RecordedTouch.x)
-                status = TouchResult.SWIPELEFT;
-            else if (CurrTouch.x >RecordedTouch.x)
-                status = TouchResult.SWIPERIGHT;
-            else
-                status = TouchResult.TAP;
-        }
+        // If there is no touch on screen
+        if (!Touching && status != TouchResult.NONE)                                    // if status is Holding/Swiping/Tap
+            ResetTouch();                                                               // Reset Touch Positions, Status and Touch on screen
+    }
+
+    public void ResetTouch()
+    {
+        CurrTouch=new Vector2(0,0);
+        RecordedTouch=new Vector2(0,0);
+        status = TouchResult.NONE;
     }
 }
