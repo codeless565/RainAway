c5be73f6026a36f968d4c003038344d006cc3672
Poh <nicholasrandomz@yahoo.com.sg>
2017-12-11 12:02:21 +0800

    Added EntityLayer
    TODO CONSTANT LAYER INT FOR UI,BKGRD

--- app/src/main/java/rainaway/sidm/com/rainaway/SampleBackGround.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/SampleBackGround.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.SurfaceView;
+import android.util.DisplayMetrics;
 
 public class SampleBackGround implements EntityBase
 {
@@ -11,6 +12,13 @@ public class SampleBackGround implements EntityBase
     private boolean isDone = false;
     private float xPos, yPos, offset;
     private SurfaceView view = null;
+    private int EntityLayer;
+    // TODO
+    // private int EntityLayer = ConstantLayout.BKGRD
+
+    // Screen resizing
+//    int ScreenWidth, ScreenHeight;
+//    private Bitmap scaledbmp = null;
 
     @Override
     public boolean IsDone() {
@@ -23,18 +31,46 @@ public class SampleBackGround implements EntityBase
     }
 
     @Override
+    public int getEntityLayer() {
+        return EntityLayer;
+    }
+
+    @Override
+    public void setEntityLayer(int _EntityLayer) {
+        EntityLayer = _EntityLayer;
+    }
+
+    @Override
     public void Init(SurfaceView _view) {
-        view = _view;
+        // TODO RESOURCE MANAGER
         bmp = BitmapFactory.decodeResource(_view.getResources(),R.drawable.gamescene);
+
+        // Retrieve information of your surface view or any device size view WEEK 9???
+//        DisplayMetrics metrics = _view.getResources().getDisplayMetrics(); // Gets the width and height based on any device
+//        ScreenWidth=metrics.widthPixels;
+//        ScreenHeight=metrics.heightPixels;
+//        scaledbmp = Bitmap.createScaledBitmap(bmp,ScreenWidth,ScreenHeight,true);
+
+        view = _view;
         offset = bmp.getWidth() * 0.2f;
+        EntityLayer=-1;
     }
 
     @Override
     public void Update(float _dt) {
-        offset -= _dt * bmp.getHeight() * 0.2f;
+        if (!Game_Normal.Instance.getIsPaused()) {
 
-        if(offset <= -bmp.getHeight() * 0.25f)
-            offset = bmp.getHeight() * 0.25f;
+            offset -= _dt * bmp.getHeight() * 0.2f;
+
+            if (offset <= -bmp.getHeight() * 0.25f)
+                offset = bmp.getHeight() * 0.25f;
+
+            // Week 9
+//        offset -= _dt * bmp.getHeight() * 0.2f;
+//
+//        if(offset <= -bmp.getHeight() * 0.25f)
+//            offset = bmp.getHeight() * 0.25f;
+        }
     }
 
     @Override
@@ -44,6 +80,9 @@ public class SampleBackGround implements EntityBase
 
         //float xOffset = (float)Math.sin(offset) * bmp.getWidth() * 0.3f; //this give the bckgnd moving back and forth
 
+        // Week 9
+        //_canvas.drawBitmap(scaledbmp,xPos,yPos,null);
+        //_canvas.drawBitmap(scaledbmp,xPos,yPos+ScreenHeight,null);
         _canvas.drawBitmap(bmp, xPos - bmp.getWidth() * 0.5f, yPos - bmp.getHeight() * 0.5f + offset, null);
     }
 
@@ -54,3 +93,4 @@ public class SampleBackGround implements EntityBase
         return result;
     }
 }
+
