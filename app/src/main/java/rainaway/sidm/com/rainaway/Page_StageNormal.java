9ee69106a989e04d17b1b82218a4e6afe4d445fa
Unknown <peisheng1@hotmail.com>
2018-01-09 22:07:18 +0800

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/rainaway/sidm/com/rainaway/Page_StageNormal.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/Page_StageNormal.java
@@ -81,6 +81,10 @@ public class Page_StageNormal extends Activity implements OnClickListener {
             intent.setClass(this, Page_StageTimeAttack.class);
             startActivity(intent);
         }
+        else if (!touching && CurrTouch.x > RecordedTouch.x) {
+            intent.setClass(this, Page_StageTimeAttack.class);
+            startActivity(intent);
+        }
 
         return true;
     }
