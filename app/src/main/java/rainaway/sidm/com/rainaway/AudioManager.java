a4c09ef8d469a9145c0d93e467b400429a28bccc
Poh <nicholasrandomz@yahoo.com.sg>
2018-01-18 13:27:46 +0800

    Added loop under Play Song

--- app/src/main/java/rainaway/sidm/com/rainaway/AudioManager.java
+++ app/src/main/java/rainaway/sidm/com/rainaway/AudioManager.java
@@ -24,13 +24,14 @@ public class AudioManager {
         view = _view;
     }
 
-    public void PlayAudio(int _id)
+    public void PlayAudio(int _id, boolean looping)
     {
         // Check if the audio is loaded or not
         if (audioMap.containsKey(_id))
         {
             // we got it
             MediaPlayer curr = audioMap.get(_id);
+            curr.setLooping(looping);
             curr.reset();
             curr.start();
         }
@@ -38,6 +39,7 @@ public class AudioManager {
         // we do not have the resource loaded therefore load them
         MediaPlayer newAudio = MediaPlayer.create(view.getContext(),_id);
         audioMap.put(_id,newAudio);
+        newAudio.setLooping(looping);
         newAudio.start();
     }
 
