0fca668206baf76095ab308c14db6bdd8de95245
PeiSheng:RE <peisheng1@hotmail.com>
2018-01-25 18:01:11 +0800

    Added Game_Data
    to retrieve data from game and give to any requestor eg. HighScore setter

---
+++ app/src/main/java/rainaway/sidm/com/rainaway/Game_Data.java
@@ -0,0 +1,63 @@
+package rainaway.sidm.com.rainaway;
+
+/**
+ * Created by 164347E on 1/25/2018.
+ */
+
+public class Game_Data {
+    public final static Game_Data Instance = new Game_Data();
+
+    //Variables
+    private float gameTime;
+    private float score;
+    private float scoreMultiplier;
+    private int enemiesDefeated;
+    private int goalPassed;
+
+    //=========================================
+    //Setters
+    //=========================================
+    public void setGameTime(float newValue)
+    {gameTime = newValue;}
+
+    public void setScore(float newValue)
+    {score = newValue;}
+
+    public void setScoreMultiplier (float newValue)
+    {scoreMultiplier = newValue;}
+
+    public void setEnemiesDefeated(int newValue)
+    {enemiesDefeated = newValue;}
+
+    public void setGoalPassed(int newValue)
+    {goalPassed = newValue;}
+
+    //=========================================
+    //Getters
+    //=========================================
+    public float getGameTime ()
+    {return gameTime;}
+
+    public float getScore ()
+    {return score;}
+
+    public float getScoreMultiplier ()
+    {return scoreMultiplier;}
+
+    public int getEnemiesDefeated()
+    {return enemiesDefeated;}
+
+    public int getGoalPassed()
+    {return goalPassed;}
+
+    //=========================================
+    //RESET DATA
+    //=========================================
+    public void resetData()
+    {
+        gameTime = 0;
+        score = 0;
+        enemiesDefeated = 0;
+        goalPassed = 0;
+    }
+}
